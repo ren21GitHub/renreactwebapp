dca555a994cde1c8114b35503617a3565a0192e3
ren21GitHub <reynielobut21@gmail.com>
2023-12-15 20:40:52 +0800

    simple route

---
+++ react/src/components/Header.jsx
@@ -0,0 +1,6 @@
+export default function Header() {
+    return(
+            <h1>Header: React Router Tutorial</h1>
+
+    )
+}
---
+++ react/src/views/NotFound.jsx
@@ -0,0 +1,10 @@
+import Header from '../components/Header'
+
+export default function NotFound() {
+    return (
+        <>
+            <Header />
+            <h2>404 - Page Not Found</h2>
+        </>
+    )
+}
